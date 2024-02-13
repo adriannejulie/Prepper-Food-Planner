b5e67c4afa4f2c0e49a74208f8b14dba7fb48088
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2024-02-13 14:28:49 -0700

    Added sign up page

--- REACT-APP/src/App.js
+++ REACT-APP/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
---
+++ REACT-APP/src/pages/SignUp.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "./SignUp.css";
+import { useState } from 'react';
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import logoPlaceholderImage from "../images/logo-placeholder-image.png";
+
+
+function SignUp() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleFNameChange = (e) => {
+        setEmail(e.target.value);
+    };
+    
+    const handleLNameChange = (e) => {
+        setEmail(e.target.value);
+    };
+    
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleReEntPassChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const signUp = () =>{
+        /*fetch(url, {
+            method: 'POST',
+            body: {
+                email: email,
+                password: password
+            }
+        }).then(response => {
+            if (response.status === 200){}
+            else{
+                window.alert("Incorrect email or password entered");
+            }
+        });*/
+    }
+
+    return (<div className="signup-container">
+    <img
+        className="placeholder-image"
+        src={logoPlaceholderImage}
+        alt="Placeholder"
+    />
+    <div className="login-google">
+        <GoogleOAuthProvider clientId="771652846868-uppchj2m6nn6dnaqu0i5g3ehv26l4cuc.apps.googleusercontent.com">
+            <GoogleLogin />
+        </GoogleOAuthProvider>
+    </div>
+    <hr className="divider" />
+
+    <div className="login-section">
+        <p>First Name</p>
+        <input
+            onChange={handleFNameChange}
+            type="text"
+        />
+    </div>    
+    <div className="login-section">
+        <p>Last Name</p>
+        <input
+            onChange={handleLNameChange}
+            type="text"
+        />
+    </div>
+    <div className="login-section">
+        <p>Email</p>
+        <input
+            onChange={handleEmailChange}
+            type="email"
+        />
+    </div>
+    <div className="login-section">
+        <p>Password</p>
+        <input
+            onChange={handlePasswordChange}
+            type="password"
+        />
+    </div>
+    <div className="login-section">
+        <p>Confirm Password</p>
+        <input
+            onChange={handleReEntPassChange}
+            type="password"
+        />
+    </div>
+    <div className="login-buttons">
+        <button onClick={signUp}>Signup</button>
+    </div>
+</div>);
+}
+
+export default SignUp;
