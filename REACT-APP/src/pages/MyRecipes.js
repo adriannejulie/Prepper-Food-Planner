e36aec9f8432ec5141d6b30db04dcc97c3ed1490
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2024-02-23 17:10:50 -0700

    my-recipes page layout
    Crated the layout of what the my recipes page will look like

--- REACT-APP/src/pages/MyRecipes.js
+++ REACT-APP/src/pages/MyRecipes.js
@@ -3,8 +3,12 @@ import "./MyRecipes.css";
 
 function MyRecipes() {
     return (
-        <div className="container">
-                MyRecipes
+        <div className="meal-planner-container">
+                <div className="recipes-container">
+                    <button className="recipe-list-show-hide">&#xe236;</button>
+                    <button className="saved-uploaded-selection">Saved Recipes</button>
+                </div>
+                <div className="recipe-viewing-container"></div>
         </div>
     );
 }
