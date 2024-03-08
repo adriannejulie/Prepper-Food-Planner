9f57041480b69db2fabcb68859ab02cd23239bb0
Nicholas Garcia <113647191+Kulaway@users.noreply.github.com>
2024-03-08 14:32:41 -0700

    basic calendar is shown. no functionality yet

--- REACT-APP/src/pages/MealPlanner.js
+++ REACT-APP/src/pages/MealPlanner.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./MealPlanner.css";
+import { LocalizationProvider, DateCalendar } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function MealPlanner() {
+
+    const [value, setValue] = useState(new Date());
+
     return (
-        <div className="container">
-                MealPlanner
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+        </LocalizationProvider> 
     );
+    // return (
+    //     <div className="container">
+    //             MealPlanner
+    //     </div>
+    // );
 }
 
 export default MealPlanner;
