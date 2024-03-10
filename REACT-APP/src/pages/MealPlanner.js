f915a1d4a1fcf0c2f9cf6df80d0ec6b80f2e5ca4
Nicholas Garcia <113647191+Kulaway@users.noreply.github.com>
2024-03-10 15:49:47 -0600

    calendar and buttons are on the page, functionality to be added

--- REACT-APP/src/pages/MealPlanner.js
+++ REACT-APP/src/pages/MealPlanner.js
@@ -3,6 +3,7 @@ import "./MealPlanner.css";
 import { LocalizationProvider, DateCalendar } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from "dayjs";
+import { MdOutlineAddBox, MdSearch } from "react-icons/md";
 
 function MealPlanner() {
 
@@ -12,10 +13,36 @@ function MealPlanner() {
         console.log(value.format("YYYY-MM-DD"));
       }, [value]);
 
+    // temp functions, functionality to be added
+    const addMeal = () => {
+        console.log("Add Meal");
+    }
+
+    const findRecipes = () => {
+        console.log("Find Recipes");
+    }
+
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
-        </LocalizationProvider> 
+        <div className="meal-planner">
+            <div className="section">
+                <div id="section-calendar">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+                    </LocalizationProvider> 
+                </div>
+                <button className="button" onClick={addMeal}>
+                    <MdOutlineAddBox />
+                    Add Meal
+                </button>
+                <button className="button" onClick={findRecipes}>
+                    <MdSearch />
+                    Find Recipes
+                </button>
+            </div>
+            <div className="section">
+                SUSSY
+            </div>
+        </div>
     );
 }
 
