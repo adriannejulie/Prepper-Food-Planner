860ee88b9f4d9d2cb5018e48f874e0caadab040e
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2024-03-09 17:38:07 -0700

    my recipe page displays recipe clicked on
    implemented the logic so that the my recipe page will display the recipe that the user has selected and updated some of the styling.

--- REACT-APP/src/components/Recipe.js
+++ REACT-APP/src/components/Recipe.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Recipe.css";
+import { MdHourglassTop } from "react-icons/md";
+import { FaBicycle } from "react-icons/fa";
 
-function Recipe({ aRecipe }) {    
+function Recipe({ aRecipe, viewNewRecipe }) {    
     return (
-        <button className="users-recipe">
+        <button onClick={() => viewNewRecipe(aRecipe)} className="users-recipe">
             <div className="recipe-name">{aRecipe.title}</div>
-            <div className="recipe-time-calories">{aRecipe.duration} Minutes</div>
-            <div className="recipe-time-calories">{aRecipe.calories} Calories</div>
+            <div className="recipe-time-calories"><MdHourglassTop className="recipe-icon-styling"/>{aRecipe.duration} Minutes</div>
+            <div className="recipe-time-calories"><FaBicycle className="recipe-icon-styling"/>{aRecipe.calories} Calories</div>
         </button>
     );
 }
