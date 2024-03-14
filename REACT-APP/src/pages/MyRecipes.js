bb9089afecfc45b7c122978a73bc6650c3947d2d
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2024-03-13 20:21:24 -0600

    edit recipe functionality works
    the recipe edit functionality works

--- REACT-APP/src/pages/MyRecipes.js
+++ REACT-APP/src/pages/MyRecipes.js
@@ -3,6 +3,7 @@ import "./MyRecipes.css";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import Recipe from "../components/Recipe.js";
 import RecipeViewing from "../components/RecipeViewing.js";
+import RecipeEditing from "../components/RecipeEditing.js";
 import { MdBookmark, MdDehaze } from "react-icons/md";
 
 function MyRecipes() {
@@ -16,7 +17,7 @@ function MyRecipes() {
         "recipeID": 1,
         "image" : "https://res.cloudinary.com/dgabkajhe/image/upload/v1709337647/Screenshot_425_asbwjt.png",
         "title" : "Buttermilk Pancakes",
-        "measuremnents" : "1 cup, 1 tsp, 1 tsp, 1, 1 1/8 cup, 2 tbsp, null",
+        "measuremnents" : "1 cup,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
         "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
         "description" : "Yummy Buttermilk pancakes",
         "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
@@ -28,7 +29,7 @@ function MyRecipes() {
         "recipeID": 2,
         "image" : "https://res.cloudinary.com/dgabkajhe/image/upload/v1709337647/Screenshot_425_asbwjt.png",
         "title" : "Buttermilk Pancakes",
-        "measuremnents" : "2 cup, 1 tsp, 1 tsp, 1, 1 1/8 cup, 2 tbsp, null",
+        "measuremnents" : "2 cup,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
         "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
         "description" : "Yummy Buttermilk pancakes",
         "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
@@ -40,7 +41,7 @@ function MyRecipes() {
         "recipeID": 3,
         "image" : "https://res.cloudinary.com/dgabkajhe/image/upload/v1709337647/Screenshot_425_asbwjt.png",
         "title" : "Buttermilk Pancakes",
-        "measuremnents" : "3 cup, 1 tsp, 1 tsp, 1, 1 1/8 cup, 2 tbsp, null",
+        "measuremnents" : "3 cup,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
         "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
         "description" : "Yummy Buttermilk pancakes",
         "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
@@ -52,7 +53,7 @@ function MyRecipes() {
         "recipeID": 4,
         "image" : "https://res.cloudinary.com/dgabkajhe/image/upload/v1709337647/Screenshot_425_asbwjt.png",
         "title" : "Buttermilk Pancakes",
-        "measuremnents" : "4 cup, 1 tsp, 1 tsp, 1, 1 1/8 cup, 2 tbsp, null",
+        "measuremnents" : "4 cup,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
         "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
         "description" : "Yummy Buttermilk pancakes",
         "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
@@ -68,7 +69,7 @@ function MyRecipes() {
             "recipeID": 1,
             "image" : "https://res.cloudinary.com/dgabkajhe/image/upload/v1709337647/Screenshot_425_asbwjt.png",
             "title" : "Buttermilk Pancake",
-            "measuremnents" : "1 cup, 1 tsp, 1 tsp, 1, 1 1/8 cup, 2 tbsp, sup",
+            "measuremnents" : "null,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
             "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
             "description" : "Yummy Buttermilk pancakes",
             "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
@@ -84,8 +85,32 @@ function MyRecipes() {
     }
 
 
-    const chanegRecipe = (recipeLookingAt) => {
-        setActiveRecipe(<RecipeViewing aRecipe={recipeLookingAt} />);
+    const editRecipe = (recipeViewing) => {
+        setActiveRecipe(<RecipeEditing aRecipe={recipeViewing} updateRecipe={updateRecipeContents}/>);
+    }
+
+
+    const selectedRecipeSaved = (recipeLookingAt) => {
+        setActiveRecipe(<RecipeViewing aRecipe={recipeLookingAt} swapToEditing={editRecipe} editAbility={false}/>);
+    }
+
+
+    const selectedRecipeUploaded = (recipeLookingAt) => {
+        setActiveRecipe(<RecipeViewing aRecipe={recipeLookingAt} swapToEditing={editRecipe} editAbility={true}/>);
+    }
+
+
+    const updateRecipeContents = (amounts, recipeIngredients, recipeTitle, cookTime, recipeCalories, recipeSteps, iDOfRecipe) => {
+        var recipes = uploadedRecipes;
+        var recipeIndex = recipes.findIndex(singleRecipe => singleRecipe.recipeID === iDOfRecipe);
+        recipes[recipeIndex].title = recipeTitle; 
+        recipes[recipeIndex].measuremnents = amounts.join(","); 
+        recipes[recipeIndex].ingredients = recipeIngredients.join(","); 
+        recipes[recipeIndex].steps = recipeSteps; 
+        recipes[recipeIndex].duration = cookTime; 
+        recipes[recipeIndex].calories = recipeCalories; 
+        setUploadedRecipes([...uploadedRecipes]);
+        setActiveRecipe(<RecipeViewing aRecipe={recipes[recipeIndex]} swapToEditing={editRecipe} editAbility={true}/>);
     }
 
 
@@ -93,10 +118,12 @@ function MyRecipes() {
         <div className="meal-planner-container">
                 <div className="recipes-container">
                     <div className="menu">
-                        <button className="recipe-list-show-hide menu-buttons"><MdDehaze className="icon-size menu-buttons"/></button>
-                        <div className="saved-uploaded-selection menu-buttons"><MdBookmark className="icon-size menu-buttons"/>{(viewingUploadedRecipes) ? "Saved Recipes" : "Uploaded Recipes"}<button className="swap-recipe-view menu-buttons" onClick={chanegRecipeView}><ArrowDropDownIcon className="menu-buttons menu-buttons" /></button></div>
+                    <button className="recipe-list-show-hide menu-buttons"><MdDehaze className="icon-size menu-buttons"/></button>
+                        <div className="saved-uploaded-selection menu-buttons"><MdBookmark className="icon-size menu-buttons"/>{(viewingUploadedRecipes) ? "Saved Recipes" : "Uploaded Recipes"}
+                            <button className="swap-recipe-view menu-buttons" onClick={chanegRecipeView}><ArrowDropDownIcon className="menu-buttons menu-buttons" /></button>
+                        </div>
                     </div>
-                    {(viewingUploadedRecipes) ? savedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={chanegRecipe} key={theRecipes.recipeID}/>)) : uploadedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={chanegRecipe} key={theRecipes.recipeID}/>))}
+                    {(viewingUploadedRecipes) ? savedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={selectedRecipeSaved} key={theRecipes.recipeID}/>)) : uploadedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={selectedRecipeUploaded} key={theRecipes.recipeID}/>))}
                 </div>
                 <div className="recipe-viewing-container">
                     {activeRecipe}
@@ -106,3 +133,11 @@ function MyRecipes() {
 }
 
 export default MyRecipes;
+
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
