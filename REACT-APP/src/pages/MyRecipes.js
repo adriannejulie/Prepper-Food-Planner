3cf61a0c30c4f0d1c15acb22c6b388490776cc96
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2024-03-16 15:01:09 -0600

    makes it so that side bar shows active recipe
    made it so that the side bar now shows which recipe the user is viewing and will make it so that it remembers what recipe the user was on if they swap from saved to uploaded

--- REACT-APP/src/pages/MyRecipes.js
+++ REACT-APP/src/pages/MyRecipes.js
@@ -11,6 +11,8 @@ function MyRecipes() {
     const [uploadedRecipes, setUploadedRecipes] = useState([]);
     const [viewingUploadedRecipes, setViewingUploadedRecipes] = useState(true);
     const [activeRecipe, setActiveRecipe] = useState("");
+    const [currentSavedRecipe, setCurrentSavedRecipe] = useState("");
+    const [currentUploadedRecipe, setCurrentUploadedRecipe] = useState("");
     
     useEffect(() => {    
         setSavedRecipes([{
@@ -76,26 +78,51 @@ function MyRecipes() {
             "duration" : "30",  
             "calories" : "210",
             "author" : "Harry Potter"
+    },
+    {
+        "recipeID": 2,
+        "image" : "https://res.cloudinary.com/dgabkajhe/image/upload/v1709337647/Screenshot_425_asbwjt.png",
+        "title" : "Buttermilk Pancake",
+        "measuremnents" : "null,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
+        "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
+        "description" : "Yummy Buttermilk pancakes",
+        "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
+        "duration" : "30",  
+        "calories" : "210",
+        "author" : "Harry Potter"
     }
     ])}, []);
 
 
     const chanegRecipeView = () => {
         setViewingUploadedRecipes(!viewingUploadedRecipes);
+        if(viewingUploadedRecipes){
+            if(currentSavedRecipe !== ""){
+                setActiveRecipe(<RecipeViewing aRecipe={currentSavedRecipe} swapToEditing={editRecipe} editAbility={false}/>);
+            }
+        }
+        else{
+            if(currentSavedRecipe !== ""){
+                setActiveRecipe(<RecipeViewing aRecipe={currentUploadedRecipe} swapToEditing={editRecipe} editAbility={true}/>);
+            }
+        }
     }
 
 
     const editRecipe = (recipeViewing) => {
+        setCurrentUploadedRecipe(recipeViewing);
         setActiveRecipe(<RecipeEditing aRecipe={recipeViewing} updateRecipe={updateRecipeContents}/>);
     }
 
 
     const selectedRecipeSaved = (recipeLookingAt) => {
+        setCurrentSavedRecipe(recipeLookingAt);
         setActiveRecipe(<RecipeViewing aRecipe={recipeLookingAt} swapToEditing={editRecipe} editAbility={false}/>);
     }
 
 
     const selectedRecipeUploaded = (recipeLookingAt) => {
+        setCurrentUploadedRecipe(recipeLookingAt);
         setActiveRecipe(<RecipeViewing aRecipe={recipeLookingAt} swapToEditing={editRecipe} editAbility={true}/>);
     }
 
@@ -110,6 +137,7 @@ function MyRecipes() {
         recipes[recipeIndex].duration = cookTime; 
         recipes[recipeIndex].calories = recipeCalories; 
         setUploadedRecipes([...uploadedRecipes]);
+        setCurrentUploadedRecipe(recipes[recipeIndex]);
         setActiveRecipe(<RecipeViewing aRecipe={recipes[recipeIndex]} swapToEditing={editRecipe} editAbility={true}/>);
     }
 
@@ -119,11 +147,11 @@ function MyRecipes() {
                 <div className="recipes-container">
                     <div className="menu">
                     <button className="recipe-list-show-hide menu-buttons"><MdDehaze className="icon-size menu-buttons"/></button>
-                        <div className="saved-uploaded-selection menu-buttons"><MdBookmark className="icon-size menu-buttons"/>{(viewingUploadedRecipes) ? "Saved Recipes" : "Uploaded Recipes"}
+                        <div className="saved-uploaded-selection menu-buttons"><MdBookmark className="icon-size menu-buttons"/>{(viewingUploadedRecipes) ? "Uploaded Recipes" : "Saved Recipes"}
                             <button className="swap-recipe-view menu-buttons" onClick={chanegRecipeView}><ArrowDropDownIcon className="menu-buttons menu-buttons" /></button>
                         </div>
                     </div>
-                    {(viewingUploadedRecipes) ? savedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={selectedRecipeSaved} key={theRecipes.recipeID}/>)) : uploadedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={selectedRecipeUploaded} key={theRecipes.recipeID}/>))}
+                    {(viewingUploadedRecipes) ? uploadedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={selectedRecipeUploaded} key={theRecipes.recipeID} isActiveRecipe={(currentUploadedRecipe.recipeID === theRecipes.recipeID)}/>)) : savedRecipes.map(theRecipes => (<Recipe aRecipe={theRecipes} viewNewRecipe={selectedRecipeSaved} key={theRecipes.recipeID} isActiveRecipe={(currentSavedRecipe.recipeID === theRecipes.recipeID)}/>))}
                 </div>
                 <div className="recipe-viewing-container">
                     {activeRecipe}
