1a7badc9327a56d8e288fd2e77ce67b353afba05
Jordan Torske <jordan.torske@ucalgary.ca>
2024-03-18 17:13:49 -0600

    Removed the arrow from account in navbar for clarity

--- REACT-APP/src/pages/Home.js
+++ REACT-APP/src/pages/Home.js
@@ -32,72 +32,68 @@ function Home() {
     }
 
     return (
-    <div className="content">
-        <img src={logoPlaceholderImage} alt="Logo" className="logo" />
-        <div className="header">
-            <div className={`${location.pathname === "/meal-planner" ? "active-link" : ""}`}>
-                Meal Planner
-                {user ? (
-                    <Link to="/meal-planner" className="link ">
-                        {location.pathname === "/meal-planner" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
-                    </Link>
-                ) : (
-                    <span className="link ">
-                        {location.pathname === "/meal-planner" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
-                    </span>
-                )}
-            </div>
-            <div className={`${location.pathname === "/my-recipes" ? "active-link" : ""}`}>
-                My Recipes
-                {user ? (
-                    <Link to="/my-recipes" className="link ">
-                        {location.pathname === "/my-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
-                    </Link>
-                ) : (
-                    <span className="link ">
-                        {location.pathname === "/my-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
-                    </span>
-                )}
-            </div>
-            <div className={`${location.pathname === "/find-recipes" ? "active-link" : ""}`}>
-                Find Recipes
-                {user ? (
-                    <Link to="/find-recipes" className="link ">
-                        {location.pathname === "/find-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
-                    </Link>
-                ) : (
-                    <span className="link ">
-                        {location.pathname === "/find-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
-                    </span>
-                )}
-            </div>
-                    <div className="account">
-                        {user ? (
-                            <>
-                                <Avatar className="avatar" alt="Profile Picture" src={user.picture} />
-                                <Menu
-                                    anchorEl={anchorEl}
-                                    open={Boolean(anchorEl)}
-                                    onClose={handleMenuClose}
-                                >
-                                    <MenuItem onClick={handleAccount}>Account</MenuItem>
-                                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                                </Menu>
-                                <p>{user.name}</p>
-                                <ArrowDropDownIcon className="dropdown-icon" onClick={handleMenuOpen} />
-
-                            </>
-                        ) : (
-                            <>
-                                <p>Account</p>
-                                <ArrowDropDownIcon className="dropdown-icon" />
-
-                            </>
-                        )}
-                    </div>
-
+        <div className="content">
+            <img src={logoPlaceholderImage} alt="Logo" className="logo" />
+            <div className="header">
+                <div className={`${location.pathname === "/meal-planner" ? "active-link" : ""}`}>
+                    Meal Planner
+                    {user ? (
+                        <Link to="/meal-planner" className="link ">
+                            {location.pathname === "/meal-planner" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
+                        </Link>
+                    ) : (
+                        <span className="link ">
+                            {location.pathname === "/meal-planner" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
+                        </span>
+                    )}
                 </div>
+                <div className={`${location.pathname === "/my-recipes" ? "active-link" : ""}`}>
+                    My Recipes
+                    {user ? (
+                        <Link to="/my-recipes" className="link ">
+                            {location.pathname === "/my-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
+                        </Link>
+                    ) : (
+                        <span className="link ">
+                            {location.pathname === "/my-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
+                        </span>
+                    )}
+                </div>
+                <div className={`${location.pathname === "/find-recipes" ? "active-link" : ""}`}>
+                    Find Recipes
+                    {user ? (
+                        <Link to="/find-recipes" className="link ">
+                            {location.pathname === "/find-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
+                        </Link>
+                    ) : (
+                        <span className="link ">
+                            {location.pathname === "/find-recipes" ? <ArrowDropDownIcon className="dropdown-icon" /> : <ArrowRightIcon className="dropdown-icon" />}
+                        </span>
+                    )}
+                </div>
+                <div className="account">
+                    {user ? (
+                        <>
+                            <Avatar className="avatar" alt="Profile Picture" src={user.picture} />
+                            <Menu
+                                anchorEl={anchorEl}
+                                open={Boolean(anchorEl)}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem onClick={handleAccount}>Account</MenuItem>
+                                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                            </Menu>
+                            <p className="dropdown-trigger" onClick={handleMenuOpen}>{user.name}</p>
+                        </>
+                    ) : (
+                        <>
+                            <p>Account</p>
+                        </>
+                    )}
+                </div>
+
             </div>
+        </div>
 
     );
 }
