da8d2bf51300293ed91619546794fa54ce5aa50e
JVanbeselaere <113467030+JVanbeselaere@users.noreply.github.com>
2024-03-20 12:08:13 -0600

    Visual adjustments

--- REACT-APP/src/pages/FindRecipes.js
+++ REACT-APP/src/pages/FindRecipes.js
@@ -12,16 +12,11 @@ function RecipeCard({ recipe, searchValue, handleClick }) {
             <img className="image" src={recipe.image}></img>
             <div className="top-container">
                 <div className="title">{recipe.title}</div>
-                <div className="author"><MdAccountBox className="icon-find"/>{recipe.author}</div>
+                <div className="author"><MdAccountBox className="icon-auth"/>{recipe.author}</div>
             </div>
             <div className="bottom-container">
-                <div>
-                    <div className="body">{recipe.description}</div>
-                </div>
-                <div className="row-container">
-                    <div className="time"><MdHourglassTop className="icon-find"/>{recipe.duration} Minutes</div>
-                    <div className="calories"><FaBicycle className="icon-find"/>{recipe.calories} Calories</div>
-                </div>
+                <div className="time"><MdHourglassTop className="icon-find"/>{recipe.duration} Minutes</div>
+                <div className="calories"><FaBicycle className="icon-find"/>{recipe.calories} Calories</div>
             </div>
         </div>
     );
@@ -39,9 +34,9 @@ function FindRecipes() {
                 "recipeID": 1,
                 "image" : "https://res.cloudinary.com/dhenatkk5/image/upload/v1710943320/qyn2z07ophdpulzskcxf.png",
                 "title" : "Buttermilk Pancakes 1",
-                "measurements" : "1 cup, 1 tsp, 1 tsp, 1, 1 1/8 cup, 2 tbsp, null",
+                "measurements" : "1 cup,1 tsp,1 tsp,1,1 1/8 cup,2 tbsp,null",
                 "ingredients" : "flour, salt, baking soda, large egg, buttermilk, butter(melted), Syrup(Optional)",
-                "description" : "Yummy Buttermilk pancakes",
+                "description" : "Yummy Buttermilk pancakes tr4heta[ojsiofndposnbvotiejokbn vbfjohgiepgjnvcbviepdojvnchb0psdnb ",
                 "steps" : "1. Preheat and grease skillet 2. Mix flour, salt, baking soda. Then add egg, buttermilk, and butter. Batter should look thick, spongy, and puffy.  3. Drop 1/3 cup of batter, spread lightly, cook until lightly browned on each side, 1-2 minutes per side.",
                 "duration" : "30",  
                 "calories" : "210",
@@ -308,7 +303,7 @@ function FindRecipes() {
             {!showRecipeViewing && (
                 <div className="box">
                     <div className="search-wrapper">
-                        <label htmlFor="search"><FaSearch className="title-icon"/>Browse Recipes</label>
+                        <label htmlFor="search" className="browse-title"><FaSearch className="title-icon"/>Browse Recipes</label>
                         <input className="find-recipe-input" type="search" id="search" value={searchValue} onChange={handleSearchChange}/>
                     </div>
                     
